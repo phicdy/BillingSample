f9fd64bc25d07ac01b9a1d582ffdda5b404a5256
Keisuke Yamaguchi <phicdy@gmail.com>
2022-10-11 15:56:34 +0900

    Add kotlin-parcelize

--- app/src/main/java/com/phicdy/billingsample2/MainState.kt
+++ app/src/main/java/com/phicdy/billingsample2/MainState.kt
@@ -1,7 +1,11 @@
 package com.phicdy.billingsample2
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class MainState(
     val subscriptionList: List<String>,
     val billingResult: String,
     val loaded: Boolean = false,
-)
+) : Parcelable
